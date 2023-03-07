fb1c90985b21008c6dfd41387b89d2ceff8d31cd
Wilson Chen <wilsonchen901@gmail.com>
2023-03-06 21:40:04 -0500

    create post form modal

--- frontend_challenge/src/components/PostModal/PostModal.jsx
+++ frontend_challenge/src/components/PostModal/PostModal.jsx
@@ -15,16 +15,17 @@ function PostModal({open, close, coffeeList}) {
             >close</span>
             </div>
             <form action="" id='post-form'>
-                <input type="text" id="post-title" placeholder='Title' />
+                <input type="text" id="post-form-title" placeholder='Title' />
                 <div id='post-form-middle'>
-                    <input type="text" id='post-rating' placeholder='Rating'/>
+                    <input type="text" id='post-rating' placeholder='Rating (1-5)'/>
                     <select id="post-coffee">
                         {coffeeList.map((coffee, idx) => (
                             <option value={coffee.name} key={idx+22}>{coffee.name}</option>
                         ))}
                     </select>
                 </div>
-                <textarea id="post-text" cols="30" rows="10"></textarea>
+                <textarea id="post-form-text" cols="30" rows="10" placeholder='Your message here.'></textarea>
+                <button>Submit</button>
             </form>
         </div>
     </>
